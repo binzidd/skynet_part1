1f49acdade869519c97c34f41eeafbaef6b37fff
Binay <binzidd@gmail.com>
2016-04-06 21:25:13 +1000

    Initial commit

---
+++ lib/comms.py
@@ -0,0 +1,81 @@
+import struct
+
+from Crypto.Cipher import XOR, AES
+from Crypto import Random
+
+from dh import dh_key_creation, calculate_dh_secret, initialisingvector
+
+class StealthConn(object):
+    def __init__(self, conn, client=False, server=False, verbose=False):
+        self.conn = conn
+        self.cipher = None
+        self.client = client
+        self.server = server
+        self.verbose = verbose
+        self.initiate_session()
+
+    def initiate_session(self):
+        # Perform the initial connection handshake for agreeing on a shared secret
+
+        ### TODO: Your code here!
+        # This can be broken into code run just on the server or just on the client
+        if self.server or self.client:
+            my_public_key, my_private_key = dh_key_creation()
+            # Send them our public key
+            self.send(bytes(str(my_public_key), "ascii"))
+            # Receive their public key
+            their_public_key = int(self.recv())
+            # Obtain our shared secret
+            shared_hash = calculate_dh_secret(their_public_key, my_private_key)
+            print("Shared hash: {}".format(shared_hash))
+
+        #  to continue from here
+        def performing_aes():
+            key= shared_hash
+            initialisingvector = IV  #NOT SURE HOW TO INVOKE
+            aes_encryption= AES.new(key,AES.MODE_CBC,initialisingvector)
+
+
+        #  passphrase MUST be 16, 24 or 32 bytes long, how can I do that ?
+       # IV = Random.new().read(BLOCK_SIZE)
+        #aes = AES.new(passphrase, AES.MODE_CFB, IV)
+        #return base64.b64encode(aes.encrypt(message))
+
+        # Implementing AES
+        self.cipher = AES.new(shared_hash[:8]) # this is 2^8 = 256 bits
+
+    def send(self, data):
+        if self.cipher:
+            encrypted_data = self.cipher.encrypt(data)
+            if self.verbose:
+                print("Original data: {}".format(data))
+                print("Encrypted data: {}".format(repr(encrypted_data)))
+                print("Sending packet of length {}".format(len(encrypted_data)))
+        else:
+            encrypted_data = data
+
+        # Encode the data's length into an unsigned two byte int ('H')
+        pkt_len = struct.pack('H', len(encrypted_data))
+        self.conn.sendall(pkt_len)
+        self.conn.sendall(encrypted_data)
+
+    def recv(self):
+        # Decode the data's length from an unsigned two byte int ('H')
+        pkt_len_packed = self.conn.recv(struct.calcsize('H'))
+        unpacked_contents = struct.unpack('H', pkt_len_packed)
+        pkt_len = unpacked_contents[0]
+
+        encrypted_data = self.conn.recv(pkt_len)
+        if self.cipher:
+            data = self.cipher.decrypt(encrypted_data)
+            if self.verbose:
+                print("Receiving packet of length {}".format(pkt_len))
+                print("Encrypted data: {}".format(repr(encrypted_data)))
+                print("Original data: {}".format(data))
+        else:
+            data = encrypted_data
+
+        return data
+
+    def close(self):
+        self.conn.close()
