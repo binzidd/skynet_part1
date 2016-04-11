af4ae07209f68b1295abaedfe908de1e55c1807e
Binay <binzidd@gmail.com>
2016-04-11 11:57:48 +1000

    Global Variable to initialise counters and public keys

--- lib/comms.py
+++ lib/comms.py
@@ -1,22 +1,22 @@
 import struct
-
 from Crypto.Cipher import AES
-from Crypto import Random
 
+from dh import aes_iv
 from dh import dh_key_creation, calculate_dh_secret
 from dh import prime
-from dh import aes_iv
+from lib import globalvariables
 
 
 class StealthConn(object):
-    def __init__(self, conn, client=False, server=False, verbose=False):
+
+    def __init__(self, conn, client=False, server=False, verbose=False):  # added status=false
         self.conn = conn
         self.cipher = None
         self.client = client
         self.server = server
         self.verbose = verbose
         self.initiate_session()
-        self.check_public_key()
+        # self.check_public_key()  #changes made
 
     def initiate_session(self):
         # Perform the initial connection handshake for agreeing on a shared secret
@@ -32,53 +32,60 @@ class StealthConn(object):
             # Obtain our shared secret
             shared_hash = calculate_dh_secret(their_public_key, my_private_key)
             print("Shared hash: {}".format(shared_hash))
-            key= shared_hash
-            iv=aes_iv()
-            #IV = SHA256.new(bytes(shared_hash, "ascii")).hexdigest()
-            self.cipher = AES.new(key,AES.MODE_CBC,iv)
+            key = shared_hash
+            iv = aes_iv()
+            self.cipher = AES.new(key, AES.MODE_CBC, iv)
             # 32 byte key length AES-256
 
-    def verify_publickey(self):
-        check_their_public_key=int(self.recv())
-        if check_their_public_key in [2,prime-1]:
-            return
-        else :
-            print("Key out of range")
-            self.
-
-
+    def verify_publickey(self):  # inserting to check our keys
+        check_their_public_key = int(self.recv())  # checking the value of thier public key
+        if check_their_public_key in [2, prime - 1]:
+            self.publickeystatus = True
+        else:
+            print("Key not in range : EVE EVE EVE incoming")
+            self.publickeystatus = False
+            # return self.publickeystatus
     def send(self, data):
-        if self.cipher:
-            encrypted_data = self.cipher.encrypt(data)
+        if self.publickeystatus == False:
+            self.conn.close()  # if the public key doesnot fall in the prime space, then connection is closed with the above message
+        else:
+            if self.cipher:
+                encrypted_data = self.cipher.encrypt(data)
             if self.verbose:
                 print("Original data: {}".format(data))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Sending packet of length {}".format(len(encrypted_data)))
-        else:
-            encrypted_data = data
-
-        # Encode the data's length into an unsigned two byte int ('H') http://www.di-mgt.com.au/cryptoCipherText.html
-        pkt_len = struct.pack('H', len(encrypted_data))
-        self.conn.sendall(pkt_len)
-        self.conn.sendall(encrypted_data)
+            else:
+                encrypted_data = data
+            # Encode the data's length into an unsigned two byte int ('H') http://www.di-mgt.com.au/cryptoCipherText.html
+            globalvariables.counter_sender +=1
+            pkt_len = struct.pack('H', len(encrypted_data))
+            self.conn.sendall(pkt_len)
+            self.conn.sendall(encrypted_data)
+            self.conn.sendall(counter_value) #implementing counter :: need to check the counter value here
 
     def recv(self):
         # Decode the data's length from an unsigned two byte int ('H')
+        globalvariables.counter_receiver+=1 #written to implement counter
         pkt_len_packed = self.conn.recv(struct.calcsize('H'))
         unpacked_contents = struct.unpack('H', pkt_len_packed)
         pkt_len = unpacked_contents[0]
-
         encrypted_data = self.conn.recv(pkt_len)
-        if self.cipher:
-            data = self.cipher.decrypt(encrypted_data)
+        counter_sender_value = self.conn.recv(globalvariables.counter_sender) #receiving the counter value
+        if counter_sender_value == globalvariables.counter_receiver : #comparing the values of counter sender and counter receiver
+            if self.cipher:
+                data = self.cipher.decrypt(encrypted_data)
             if self.verbose:
                 print("Receiving packet of length {}".format(pkt_len))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Original data: {}".format(data))
-        else:
-            data = encrypted_data
+            else:
+                data = encrypted_data
+            return data
+        else :
+            print("replay attack detected")
+            self.conn.close
 
-        return data
 
     def close(self):
         self.conn.close()
